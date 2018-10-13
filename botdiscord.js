b42d67a9a64e93018dbdc7607161a3f6f7db4b99
karzo8s <44073963+karzo8s@users.noreply.github.com>
2018-10-13 20:46:54 +0200

    Update botdiscord.js

--- botdiscord.js
+++ botdiscord.js
@@ -555,25 +555,7 @@ channel.send(`${member} ولكم يا عمو البوت`)
 
  
  
- 
 
-
-client.on('message', message => {
-   if(message.content.startsWith(prefix + "invites")) {
-    message.guild.fetchInvites().then(invs => {
-      let user = message.mentions.users.first() || message.author
-      let personalInvites = invs.filter(i => i.inviter.id === user.id);
-      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
-               let mmmmEmbed = new Discord.RichEmbed()
-                         .setAuthor(client.user.username)
-                         .setThumbnail(message.author.avatarURL)
-channel.send(تم دعوته بواسطة  ${Invite.inviter}) ;
-           .setFooter(`- Requested By: ${message.author.tag}`);
-           message.channel.send(mmmmEmbed)
-});
-  }
-});
- 
  
  
  
