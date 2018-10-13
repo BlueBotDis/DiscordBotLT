c1563047d48278354debb4788300fc6f53451394
karzo8s <44073963+karzo8s@users.noreply.github.com>
2018-10-13 18:53:12 +0200

    Update botdiscord.js

--- botdiscord.js
+++ botdiscord.js
@@ -1496,21 +1496,40 @@ client.on('guildMemberAdd', member => {
 });
  
  
-client.on('message', message => {
-    let actrole = message.guild.roles.find('name', "✦ Member")
-    let user = message.mentions.members.first()
-    if(message.content.startsWith(prefix + "act")){
-        message.guild.member(user).addRole(actrole);
-        var embed = new Discord.RichEmbed()
-        .setTitle(`Activated!`)
-        .setThumbnail(user.avatarURL)
-        .addField(`User Activated!`, `${user} Was Activated By <@${message.author.id}>`)
-        .setColor("RANDOM")
-        .setTimestamp()
-        .setFooter(" ")
-        message.channel.send({embed})
-    }
+
+client.on('guildMemberAdd', (member) => {
+member.addRole(member.guild.roles.find('name', 'not active'));
 });
+
+
+client.on('message', message => {                      
+    if(!message.channel.guild) return;
+       if(message.content.startsWith(prefix + 'active')) {
+        let modlog = client.channels.find('name', 'الـــــــــشات_العام');
+       if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+       message.channel.sendMessage(`اضغط على الصح عشان تتفعل`).then(msg => {
+        
+        
+        msg.react('✅')
+       .then(() => msg.react('✅'))
+     
+     
+
+       let activeFilter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+     
+       let active = msg.createReactionCollector(activeFilter, { time: 15000 });
+     
+                                                        
+                               active.on("collect", r => {
+                                   message.member.addRole(message.guild.roles.find("name", "active"));
+                                   message.member.removeRole(message.guild.roles.find("name", "not active"));
+                                   msg.delete();
+                                   message.channel.send(`**تم تفعيلك استمتع.**`).then(m => m.delete(1000));
+     
+                                   })
+                                   })
+                                   }
+                                   });
 
 client.on('message', message => {
   if(message.content.startsWith("#credit <@الايدي حقك هنا>","#credits <@الايدي حقك هنا>")) {
