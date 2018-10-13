fab4196e961b4f3d387d77fbc76901fc792552f2
karzo8s <44073963+karzo8s@users.noreply.github.com>
2018-10-13 14:36:59 +0200

    Update botdiscord.js

--- botdiscord.js
+++ botdiscord.js
@@ -427,11 +427,11 @@ client.on('message',async message => {
  
  
 client.on('ready',async () => {
-let streaming = [`${prefix}help💙💚 `, `Legend Bot 💚💙 `, `Bot: 0.1 Beta💚💙`];
+let streaming = [`${prefix}help💙💚 `, `Legend Bot 💚💙 `, `Bot: 0.1 Beta💚💙` , `!رابط link support server`];
 client.user.setActivity(streaming[Math.floor(Math.random() * streaming.length)], {type: 1, url: "https://twitch.tv/6xlez1"});
 setInterval(() => {
 client.user.setActivity(streaming[Math.floor(Math.random() * streaming.length)], {type: 1, url: "https://twitch.tv/6xlez1"});
-}, 2500);
+}, 1500);
 });
  
  
