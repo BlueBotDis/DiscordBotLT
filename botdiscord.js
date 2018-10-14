8d2cb3b0950a855006dbf0558596681d16967926
karzo8s <44073963+karzo8s@users.noreply.github.com>
2018-10-14 22:05:22 +0200

    Update botdiscord.js

--- botdiscord.js
+++ botdiscord.js
@@ -1238,7 +1238,7 @@ client.on('guildMemberAdd', member => {
                   .addField("Name:",`<@` + `${member.id}` + `>`, true)
                       
                                      .addField(' الـسيرفر', `${member.guild.name}`,true)
-    .addField('مدة انشاء حسابك', member.user.createdAt.toLocaleString(), true);
+    .addField('مدة انشاء حسابك', member.user.createdAt.toLocaleString(), true)
 
                                        
      .setFooter("LGEGND TIME :L")
