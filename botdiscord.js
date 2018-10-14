a17f414682362e6e6f12595664607fa67f58b3ee
karzo8s <44073963+karzo8s@users.noreply.github.com>
2018-10-14 23:17:22 +0200

    Update botdiscord.js

--- botdiscord.js
+++ botdiscord.js
@@ -1521,7 +1521,7 @@ if (message.content.startsWith(adminprefix + 'setgame')) {
   if (message.content.startsWith(adminprefix + 'setname')) {
 client.user.setUsername(argresult).then
     message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
-return message.reply("**${argresult}** : تم تغيير أسم البوت إلى **");
+return message.reply("** تم تغيير أسم البوت  **");
 } else
   if (message.content.startsWith(adminprefix + 'setavatar')) {
 client.user.setAvatar(argresult);
