1f8700b214b04cc65c0f99ad397d7e8ad8194987
karzo8s <44073963+karzo8s@users.noreply.github.com>
2018-10-15 22:31:56 +0200

    Update botdiscord.js

--- botdiscord.js
+++ botdiscord.js
@@ -1564,10 +1564,7 @@ client.on('message', message => {
                         })
  
  
-                    })
- 
- 
-           
+                    }) 
     }
 });
 client.login(process.env.BOT_TOKEN);
