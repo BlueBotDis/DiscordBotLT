fe3edb614846a2ce6f4b6f9d0579fbbc0fdaa381
karzo8s <44073963+karzo8s@users.noreply.github.com>
2018-10-15 23:52:21 +0200

    Update botdiscord.js

--- botdiscord.js
+++ botdiscord.js
@@ -1346,11 +1346,26 @@ client.on('message',async message => {
   });
  
  
- 
- 
- 
- 
- 
+client.on('ready',async () => {
+  let GUILDID = '498078431972556800'; // اي دي السيرفر
+  let CHANNELID = '501512478392320010'; // اي دي الروم
+  voiceStay(GUILDID, CHANNELID);
+  function voiceStay(guildid, channelid) {
+    if(!guildid) throw new Error('Syntax: voiceStay function requires guildid');
+    if(!channelid) throw new Error('Syntax: voiceStay function requires channelid');
+
+    let guild = client.guilds.get(guildid);
+    let channel = guild.channels.get(channelid);
+
+    if(channel.type === 'voice') {
+      channel.join().catch(e => {
+        console.log(`Failed To Join :: ${e.message}`);
+      });
+    } else {
+      console.log(`Channel Type :: ${channel.type}, It must be Voice.`);
+    }
+  }
+});
  
  
  
