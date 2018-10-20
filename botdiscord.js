77f0ce2e337643f9de8b2109fa0a1ac9a24535bb
karzo8s <44073963+karzo8s@users.noreply.github.com>
2018-10-20 12:52:47 +0200

    Update botdiscord.js

--- botdiscord.js
+++ botdiscord.js
@@ -95,8 +95,8 @@ client.on('message', message => {
             if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('!bcall')){
- if (message.author.id !== 'ايديك') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
- if(!message.author.id === 'ايديك') return;
+ if (message.author.id !== '427855446225846272') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+ if(!message.author.id === '427855446225846272') return;
 message.channel.sendMessage('جار ارسال الرسالة |✅')
 client.users.forEach(m =>{
 m.sendMessage(args)
