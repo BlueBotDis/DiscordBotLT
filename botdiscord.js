5f6b672626d9d86df5cbfa8377a3d95ac402515c
karzo8s <44073963+karzo8s@users.noreply.github.com>
2018-11-03 02:03:08 +0200

    Update botdiscord.js

--- botdiscord.js
+++ botdiscord.js
@@ -532,37 +532,7 @@ if(!ReBeL.voiceChannel) {
 });
  
  
-client.on('message', async message => {
-  if(message.content.startsWith(prefix + "sug")) {
-  await  message.channel.send(`اكتب اقتراحك الان`)
-    let filter = m => m.author.id === message.author.id
-      var text = '';
-        let sugsa = message.channel.awaitMessages(filter, { max: 1, time: 60000})
-          .then(co => {
-            text = co.first().content
 
-              message.channel.send(`تم حفظ اقتراحك الرجاء انتضار الرد من قبل الاداره`)
-                client.channels.get("501832707764912138").send(`${message.author.username}'s sug => ${text}`)
-
-              })
-            }
-          }) 
-
-client.on('message', async message => {
-  if(message.content.startsWith(prefix + "sub")) {
-  await  message.channel.send(`اكتب الرتبة التي ستقدم لها`)
-    let filter = m => m.author.id === message.author.id
-      var text = '';
-        let sugsa = message.channel.awaitMessages(filter, { max: 1, time: 60000})
-          .then(co => {
-            text = co.first().content
-
-              message.channel.send(`تم حفظ تقديمك الرجاء انتضار الرد من قبل الاداره`)
-                client.channels.get("508053414165348355").send(`${message.author.username}'s sug => ${text}`)
-
-              })
-            }
-          }) 
  
  
  
